1a44993adbfa21d2aeb2fc21dd573ef5eb6121e3
Michael Irwin <mike.irwin1@gmail.com>
2019-01-09 19:16:27 +0000

    isogram complete

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,22 @@
 const IsogramFinder = function (word) {
-
+  this.word = word.toLowerCase().split('');
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+  const charCounter = {};
+  let isogram = true;
+  this.word.forEach((letter) => {
+    if (charCounter[letter]) {
+      charCounter[letter] += 1;
+    }
+    else {
+      charCounter[letter] = 1;
+    }
+    if (charCounter[letter] > 1){
+      isogram = false;
+    }
+  })
+  return isogram;
 }
 
 module.exports = IsogramFinder;
