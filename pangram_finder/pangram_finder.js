7abcbcb644f2b0d1012327bb3ae5f83ad0997198
Michael Irwin <mike.irwin1@gmail.com>
2019-01-09 18:37:50 +0000

    pangram solved

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,22 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase().split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
+  const checkPangram =  this.alphabet.every((letter) => {
+     return this.phrase.includes(letter)
+  });
 
-}
+  return checkPangram;
+
+  // const uniqueLetters = new Set(this.phrase.toLowerCase())
+  // if (uniqueLetters.size === 26) {
+  //   return true;
+  // }
+  // else {
+  //   return false;
+  // }
+};
 
 module.exports = PangramFinder;
