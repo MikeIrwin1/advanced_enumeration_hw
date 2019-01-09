15f64df6d5f30fb7013824a076838bfe266dba04
Michael Irwin <mike.irwin1@gmail.com>
2019-01-09 21:24:08 +0000

    can detect anagram

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,24 @@
 const AnagramFinder = function (word) {
-
+  this.word = word;
 }
+
+// AnagramFinder.prototype.parseArray = function (array) {
+//   return array.map((word) => {
+//     return word.split('').sort().join('');
+//   })
+// };
+
+AnagramFinder.prototype.parseWord = function (word) {
+  return word.split('').sort().join('');
+};
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
 
+  let parsedWord = this.parseWord(this.word);
+  let results = [];
+  return otherWords.filter((word) => {
+    return this.parseWord(word) === parsedWord;
+  })
 }
 
 module.exports = AnagramFinder;
